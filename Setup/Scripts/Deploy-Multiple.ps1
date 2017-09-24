429ecfc1c27afd4678372c4499e0c166caa47f25
Zoiner Tejada <zoinertejada@solliance.net>
2017-09-23 21:23:28 -0400

    added support for deployment of multiple environments

--- Setup/Scripts/Deploy-Multiple.ps1
+++ Setup/Scripts/Deploy-Multiple.ps1
@@ -15,7 +15,7 @@
 #$clusterName = 'hdilabs'
 #$numInstances = 2
 
-
+$destContainerName = "hdi-labs"
 $sourceFolder = Get-Location
 $cred = Get-Credential
 $jobs = @()
@@ -24,9 +24,9 @@ For($i = 1; $i -le $numInstances; $i++){
     $resourceGroupInstanceName = $resourceGroupName +$i
     
 
-    $params = @($clusterInstanceName, $resourceGroupInstanceName, $resourceGroupLocation, $subscriptionId, $sourceFolder, $cred) 
+    $params = @($clusterInstanceName, $resourceGroupInstanceName, $resourceGroupLocation, $subscriptionId, $sourceFolder, $destContainerName, $cred) 
     $job = Start-Job -ScriptBlock { 
-        param($clusterInstanceName, $resourceGroupInstanceName, $resourceGroupLocation, $subscriptionId, $sourceFolder, $cred) 
+        param($clusterInstanceName, $resourceGroupInstanceName, $resourceGroupLocation, $subscriptionId, $sourceFolder, $destContainerName, $cred) 
         try{
             Get-AzureRmSubscription
         }
@@ -37,6 +37,14 @@ For($i = 1; $i -le $numInstances; $i++){
         Write-Host("Deploying instance " + $clusterInstanceName + " in Resource Group " + $resourceGroupInstanceName + " in subscription " + $sub.Subscription.SubscriptionName + " (" + $sub.Subscription.SubscriptionId + ")")
         Set-Location $sourceFolder
         .\Deploy-AzureResourceGroup.ps1 -ResourceGroupName $resourceGroupInstanceName -ResourceGroupLocation $resourceGroupLocation -TemplateFile 'azuredeploy.json' -TemplateParametersFile 'azuredeploy.parameters.json' -ClusterName $clusterInstanceName 
+        $storageAccountName = $clusterInstanceName
+        Select-AzureRmSubscription -SubscriptionId $subscriptionId
+        Set-AzureRmCurrentStorageAccount -ResourceGroupName $resourceGroupInstanceName -Name $storageAccountName
+        New-AzureStorageContainer -Name $destContainerName -Permission Off
+        $storageKey = (Get-AzureRmStorageAccountKey -Name $storageAccountName -ResourceGroupName $resourceGroupInstanceName).Value[0]
+        $sourceSAS = "'?sv=2017-04-17&ss=b&srt=co&sp=rl&se=2019-12-31T18:29:33Z&st=2017-09-18T10:29:33Z&spr=https&sig=bw1EJflDFx9NuvLRdBGql8RU%2FC9oz92Dz8Xs76cftJM%3D'"
+        az storage blob copy start-batch --source-account-name retaildatasamples --source-container data   --account-name $storageAccountName --account-key $storageKey --destination-container $destContainerName --pattern retaildata/rawdata* --source-sas ${sourceSAS}
+
     } -ArgumentList $params  
     $jobs = $jobs + $job 
 }
